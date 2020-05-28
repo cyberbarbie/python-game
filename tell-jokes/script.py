5b3c27d757fc6d081b13004ff258f441fd18ca4f
cyberbarbie <taeluralexis@gmail.com>
2020-05-28 05:04:56 -0400

    Added a joke telling game

---
+++ tell-jokes/script.py
@@ -0,0 +1,21 @@
+print("What do you get when you cross a snowman with a vampire?")
+input() # add an empty input so it demands user input to not spoil joke
+print("Frostbite!")
+print() # print a blank line
+print("What do dentists call an astronaut's cavity?")
+input()
+print("A Black Hole!")
+print()
+print("Knock knock.")
+input()
+print("Who's there?")
+input()
+print("Interrupting cow wh", end = '')
+print('-MOO!')
+
+# end = '' - print argument that adds a blank string instead of a new line
+# \\ backslash
+# \' single quote
+# \" double quote
+# \n newline
+# \t tab
